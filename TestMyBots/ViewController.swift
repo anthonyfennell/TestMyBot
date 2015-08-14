54855784939acab721425b6e74dec7b706f6c0a8
Anthony Fennell <anthony@outsidesource.com>
2015-08-14 09:38:09 -0400

    Initial commit

---
+++ TestMyBots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMyBots
+//
+//  Created by Anthony Michael Fennell on 8/14/15.
+//  Copyright © 2015 Ford Prefect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
